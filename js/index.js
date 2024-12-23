410dd10cc27e039580daf2d92da3acde0e389c36
Sagar Kumar <s4g4r.dev@gmail.com>
2024-12-23 22:50:53 +0530

    - Project Sectin Update - Partial project section implement from json - Runtime generation of Html from js.

--- js/index.js
+++ js/index.js
@@ -51,12 +51,112 @@ function submitForm(event) {
 }
 
 
-const jsonURL = 'http://127.0.0.1:3000/json/BlogData.json'; 
+// const jsonURL = 'http://127.0.0.1:3000/json/BlogData.json';
+
+// const xhr = new XMLHttpRequest();
+
+// // Configure it: GET-request for the URL
+// xhr.open('GET', jsonURL, true);
+
+// // Set up a function to handle the response
+// xhr.onload = function () {
+//   if (xhr.status >= 200 && xhr.status < 300) {
+//     try {
+//       // Parse the JSON response
+//       const data = JSON.parse(xhr.responseText);
+//       console.log('JSON Data:', data);
+
+//       const parentElement = document.getElementById('blogContainer');
+//       if (!parentElement) {
+//         console.error('No element with ID "blog" found.');
+//         return;
+//       }
+
+//       // Example: Accessing the articles and their details
+//       data.forEach(item => {
+
+// // Create a time element
+// const divElement = document.createElement('div');
+// divElement.classList.add('blog-item');
+
+// const timeElement = document.createElement('time');
+// timeElement.setAttribute('datetime', item.Date); // Set the datetime attribute
+// timeElement.textContent = item.Date;
+// timeElement.classList.add('blog-time');// Set the content
+
+// // Create an h3 element for the title
+// const h3Element = document.createElement('h3');
+// h3Element.textContent = item.Title;
+// h3Element.classList.add('blog-title');
+
+// // Create an anchor (a) element for the link
+// const aElement = document.createElement('a');
+// aElement.setAttribute('href', item.Link); // Set the href attribute
+// aElement.textContent = 'Read more'; // Link text
+// aElement.classList.add('blog-link');
+
+// // Append the new elements to the parent
+// divElement.appendChild(timeElement);
+// divElement.appendChild(h3Element);
+// divElement.appendChild(aElement);
+
+// parentElement.appendChild(divElement);
+
+
+
+//         console.log(`Title: ${item.Title}`);
+//         console.log(`Link: ${item.Link}`);
+//         item.TextDescription.forEach(description => {
+//           console.log(`Paragraph: ${description.paragraph}`);
+//         });
+//         item.Images.forEach(image => {
+//           console.log(`Image URL: ${image.url}`);
+//           console.log(`Alt Text: ${image.alt}`);
+//         });
+//       });
+//     } catch (error) {
+//       console.error('Error parsing JSON:', error);
+//       jsonParsingIssue();
+//     }
+//   } else {
+//     console.error(`HTTP Error: ${xhr.status}`);
+//   }
+// };
+
+// // Handle network errors
+// xhr.onerror = function () {
+//   console.error('Network Error');
+
+//   jsonParsingIssue();
+// };
+
+// // Send the request
+// xhr.send();
+
+
+// function jsonParsingIssue()
+// {
+//   // Find the element with the ID 'blog' and set its display to 'none'
+//   const blogElement = document.getElementById('blog');
+//   if (blogElement) {
+//     blogElement.style.display = 'none';
+//     console.log('Element with ID "blog" hidden due to error.');
+//   } else {
+//     console.error('No element with ID "blog" found.');
+//   }
+// }
+
+
+
+
+
+/*============================================================================== */
+const projectJSONURL = 'http://127.0.0.1:3000/json/ProjectData.json';
 
 const xhr = new XMLHttpRequest();
 
 // Configure it: GET-request for the URL
-xhr.open('GET', jsonURL, true);
+xhr.open('GET', projectJSONURL, true);
 
 // Set up a function to handle the response
 xhr.onload = function () {
@@ -66,7 +166,7 @@ xhr.onload = function () {
       const data = JSON.parse(xhr.responseText);
       console.log('JSON Data:', data);
 
-      const parentElement = document.getElementById('blogContainer');
+      const parentElement = document.getElementById('projectContainer');
       if (!parentElement) {
         console.error('No element with ID "blog" found.');
         return;
@@ -75,48 +175,59 @@ xhr.onload = function () {
       // Example: Accessing the articles and their details
       data.forEach(item => {
 
-// Create a time element
-const divElement = document.createElement('div');
-divElement.classList.add('blog-item');
-
-const timeElement = document.createElement('time');
-timeElement.setAttribute('datetime', item.Date); // Set the datetime attribute
-timeElement.textContent = item.Date; 
-timeElement.classList.add('blog-time');// Set the content
-
-// Create an h3 element for the title
-const h3Element = document.createElement('h3');
-h3Element.textContent = item.Title;
-h3Element.classList.add('blog-title'); 
-
-// Create an anchor (a) element for the link
-const aElement = document.createElement('a');
-aElement.setAttribute('href', item.Link); // Set the href attribute
-aElement.textContent = 'Read more'; // Link text
-aElement.classList.add('blog-link');
-
-// Append the new elements to the parent
-divElement.appendChild(timeElement);
-divElement.appendChild(h3Element);
-divElement.appendChild(aElement);
-
-parentElement.appendChild(divElement);
+        // Create a time element
+        const divElement = document.createElement('div');
+        const h3Element = document.createElement('h3');
+        const h4Element = document.createElement('h4');
 
 
+        h3Element.textContent = 'Featured Project';
+        h4Element.textContent = item.Title;
 
-        console.log(`Title: ${item.Title}`);
-        console.log(`Link: ${item.Link}`);
-        item.TextDescription.forEach(description => {
-          console.log(`Paragraph: ${description.paragraph}`);
+        const boxDivElement = document.createElement('div');
+        const para = document.createElement('p');
+        para.textContent = item.Description;
+        boxDivElement.appendChild(para);
+
+        const stackElement = document.createElement('ul');
+        item.Stack.forEach(subItem => {
+          const stackSubElement = document.createElement('li');
+          const stackSpanElement= document.createElement('span');
+          stackSpanElement.textContent= subItem.Name;
+
+          stackSubElement.appendChild(stackSpanElement);
+          stackElement.appendChild(stackSubElement);
         });
-        item.Images.forEach(image => {
-          console.log(`Image URL: ${image.url}`);
-          console.log(`Alt Text: ${image.alt}`);
+
+
+        const projectElement = document.createElement('ul');
+        item.links.forEach(subItem => {
+          const stackSubElement = document.createElement('li');
+          const stackHrefElement= document.createElement('a');
+          stackHrefElement.href = subItem.Link;
+          stackHrefElement.target = "_blank";
+
+          const stackSpanElement = document.createElement('span');
+          stackSpanElement.classList.add('bi-'+subItem.Icon);
+          
+          stackHrefElement.appendChild(stackSpanElement);
+          stackSubElement.appendChild(stackHrefElement);
+          projectElement.appendChild(stackSubElement);
         });
+
+
+        divElement.appendChild(h3Element);
+        divElement.appendChild(h4Element);
+        divElement.appendChild(boxDivElement);
+        divElement.appendChild(stackElement);
+        divElement.appendChild(projectElement);
+
+        parentElement.appendChild(divElement);
+
       });
     } catch (error) {
       console.error('Error parsing JSON:', error);
-      jsonParsingIssue();
+      projectJsonParsingIssue();
     }
   } else {
     console.error(`HTTP Error: ${xhr.status}`);
@@ -126,23 +237,21 @@ parentElement.appendChild(divElement);
 // Handle network errors
 xhr.onerror = function () {
   console.error('Network Error');
-  
-  jsonParsingIssue();
+
+  projectJsonParsingIssue();
 };
 
 // Send the request
 xhr.send();
 
 
-function jsonParsingIssue()
-{
+function projectJsonParsingIssue() {
   // Find the element with the ID 'blog' and set its display to 'none'
-  const blogElement = document.getElementById('blog');
-  if (blogElement) {
-    blogElement.style.display = 'none';
+  const projectElement = document.getElementById('project');
+  if (projectElement) {
+    projectElement.style.display = 'none';
     console.log('Element with ID "blog" hidden due to error.');
   } else {
     console.error('No element with ID "blog" found.');
   }
-
 }
